3e2ddf8c6e5870b2dae8815e3f4a199da0d76098
HaiTrieu <trieuphan3701@gmail.com>
2023-11-08 10:48:41 +0700

    push code

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./assest/global.css" />
+    <link rel="stylesheet" href="./assest/style.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" />
+</head>
+
+<body>
+    <div class="todo">
+        <div class="todo-header"></div>
+        <div class="menu"></div>
+        <img class="todo-content" alt="" src="./public/rectangle-3.svg" />
+
+        <div class="todo-emoji-5"></div>
+        <!-- <div class="rectangle-div"></div> -->
+        <div class="todo-header-content"></div>
+        <div class="todo-work-1"></div>
+        <div class="todo-water1"></div>
+        <div class="todo-water2"></div>
+        <div class="todo-water3"></div>
+        <div class="todo-water4"></div>
+        <div class="todo-water5"></div>
+        <div class="todo-emoji-1"></div>
+        <div class="todo-work-label"></div>
+        <b class="vic-cn-lm">Việc cần làm</b>
+        <div class="todo-emoji-label"></div>
+        <b class="cm-xc">Cảm Xúc</b>
+        <div class="todo-water-label"></div>
+        <b class="vic-cn-lm1">Lượng nước uống</b>
+        <b class="todo-text-work-1"> Công việc 1</b>
+        <img class="checked-checkbox-icon" alt="" src="./public/checked-checkbox@2x.png" />
+
+        <div class="todo-work-2"></div>
+        <b class="todo-text-work-2"> Công việc 2</b>
+        <img class="checked-checkbox-icon1" alt="" src="./public/checked-checkbox1@2x.png" />
+
+        <div class="todo-work-3"></div>
+        <b class="todo-text-work-3"> Công việc 3</b>
+        <img class="checked-checkbox-icon2" alt="" src="./public/checked-checkbox2@2x.png" />
+
+        <div class="todo-work-4"></div>
+        <b class="todo-text-work-4"> Công việc 4</b>
+        <img class="checked-checkbox-icon3" alt="" src="./public/checked-checkbox3@2x.png" />
+
+        <div class="todo-work-5"></div>
+        <b class="todo-text-work-5"> Công việc 5</b>
+        <img class="checked-checkbox-icon4" alt="" src="./public/checked-checkbox4@2x.png" />
+
+        <div class="todo-work-6"></div>
+        <div class="todo-note"></div>
+        <b class="todo-text-work-6"> Công việc 6</b>
+        <img class="checked-checkbox-icon5" alt="" src="./public/checked-checkbox5@2x.png" />
+
+        <img class="image-6-icon" alt="" src="./public/image-6@2x.png" />
+
+        <div class="todo-emoji-2"></div>
+        <div class="todo-emoji-3"></div>
+        <div class="todo-emoji-4"></div>
+        <img class="image-133-icon" alt="" src="./public/image-133@2x.png" />
+
+        <img class="image-158-icon" alt="" src="./public/image-158@2x.png" />
+
+        <b class="hnh-phc">Hạnh phúc</b>
+        <b class="hnh-phc">Hạnh phúc</b>
+        <b class="vui">Vui</b>
+        <b class="bnh-thng">Bình thường</b>
+        <b class="bun">Buồn</b>
+        <b class="rt-bun">Rất buồn</b>
+        <b class="lit">0,5 Lit</b>
+        <b class="lit1">1 Lit</b>
+        <b class="lit2">1,5 Lit</b>
+        <b class="lit3">2 Lit</b>
+        <b class="lit4">2,5 Lit</b>
+        <img class="done-icon" alt="" src="./public/done@2x.png" />
+
+        <b class="todo-text-work-trong">Công việc trong ngày</b>
+        <div class="todo-note-label"></div>
+        <b class="ghi-ch">Ghi chú :</b>
+        <img class="line-icon" alt="" src="./public/line-1.svg" />
+
+        <img class="todo-work-12" alt="" src="./public/line-2.svg" />
+
+        <img class="todo-work-13" alt="" src="./public/line-3.svg" />
+
+        <img class="sparkling-water-icon" alt="" src="./public/sparkling-water@2x.png" />
+
+        <img class="image-27-icon" alt="" src="./public/image-27@2x.png" />
+
+        <img class="image-20-icon" alt="" src="./public/image-20@2x.png" />
+
+        <img class="sparkling-water-icon1" alt="" src="./public/sparkling-water1@2x.png" />
+
+        <img class="sparkling-water-icon2" alt="" src="./public/sparkling-water2@2x.png" />
+
+        <img class="sparkling-water-icon3" alt="" src="./public/sparkling-water3@2x.png" />
+
+        <img class="sparkling-water-icon4" alt="" src="./public/sparkling-water4@2x.png" />
+
+        <img class="plus-icon" alt="" src="./public/plus@2x.png" />
+    </div>
+</body>
+
+</html>
---
+++ index_todo.php
@@ -0,0 +1,171 @@
+<div class="Todo" style="width: 1440px; height: 1024px; position: relative; background: #E9E9E9">
+    <div class="Rectangle1"
+        style="width: 1440px; height: 64px; left: 0px; top: 0px; position: absolute; background: #9575CD"></div>
+    <div class="Menu" style="width: 280px; height: 960px; left: 0px; top: 64px; position: absolute; background: white">
+    </div>
+    <div class="Rectangle3"
+        style="width: 1100px; height: 910.34px; left: 310px; top: 90px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)">
+    </div>
+    <div class="Rectangle20"
+        style="width: 334px; height: 67px; left: 691px; top: 620px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #848484 solid">
+    </div>
+    <div class="Rectangle4"
+        style="width: 280px; height: 63px; left: 0px; top: 98px; position: absolute; background: #D9D9D9"></div>
+    <div class="Rectangle5"
+        style="width: 1100px; height: 106px; left: 310px; top: 90px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15); border-bottom: 1px rgba(0, 0, 0, 0.80) solid">
+    </div>
+    <div class="Rectangle6"
+        style="width: 334px; height: 66px; left: 337px; top: 272px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #969696 solid">
+    </div>
+    <div class="Rectangle7"
+        style="width: 334px; height: 67px; left: 1045px; top: 272px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #848484 solid">
+    </div>
+    <div class="Rectangle21"
+        style="width: 334px; height: 67px; left: 1045px; top: 360px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #848484 solid">
+    </div>
+    <div class="Rectangle22"
+        style="width: 334px; height: 67px; left: 1045px; top: 447px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #848484 solid">
+    </div>
+    <div class="Rectangle23"
+        style="width: 334px; height: 67px; left: 1045px; top: 533px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #848484 solid">
+    </div>
+    <div class="Rectangle24"
+        style="width: 334px; height: 67px; left: 1045px; top: 621px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #848484 solid">
+    </div>
+    <div class="Rectangle8"
+        style="width: 334px; height: 67px; left: 691px; top: 272px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #848484 solid">
+    </div>
+    <div class="Rectangle9"
+        style="width: 108px; height: 34px; left: 337px; top: 216px; position: absolute; background: rgba(248.40, 235.79, 122.34, 0.77); box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15); border-radius: 8px">
+    </div>
+    <div class="ViCCNLM"
+        style="width: 88px; height: 34px; left: 347px; top: 216px; position: absolute; text-align: center; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Việc cần làm</div>
+    <div class="Rectangle10"
+        style="width: 109px; height: 34px; left: 691px; top: 216px; position: absolute; background: rgba(252.45, 158.85, 158.85, 0.55); box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15); border-radius: 8px">
+    </div>
+    <div class="CMXC"
+        style="width: 89px; height: 34px; left: 701px; top: 216px; position: absolute; text-align: center; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Cảm Xúc</div>
+    <div class="Rectangle11"
+        style="width: 149px; height: 34px; left: 1045px; top: 216px; position: absolute; background: rgba(122.34, 248.40, 203.02, 0.77); box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15); border-radius: 8px">
+    </div>
+    <div class="ViCCNLM"
+        style="width: 129px; height: 34px; left: 1055px; top: 216px; position: absolute; text-align: center; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Lượng nước uống</div>
+    <div class="CNgViC1"
+        style="width: 334px; height: 67px; left: 337px; top: 272px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Công việc 1</div>
+    <img class="CheckedCheckbox" style="width: 40px; height: 50.83px; left: 344px; top: 279px; position: absolute"
+        src="https://via.placeholder.com/40x51" />
+    <div class="Rectangle12"
+        style="width: 334px; height: 66px; left: 337px; top: 359px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #969696 solid">
+    </div>
+    <div class="CNgViC2"
+        style="width: 334px; height: 67px; left: 337px; top: 359px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Công việc 2</div>
+    <img class="CheckedCheckbox" style="width: 40px; height: 50.83px; left: 344px; top: 366px; position: absolute"
+        src="https://via.placeholder.com/40x51" />
+    <div class="Rectangle13"
+        style="width: 334px; height: 66px; left: 337px; top: 446px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #969696 solid">
+    </div>
+    <div class="CNgViC3"
+        style="width: 334px; height: 67px; left: 337px; top: 446px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Công việc 3</div>
+    <img class="CheckedCheckbox" style="width: 40px; height: 50.83px; left: 344px; top: 453px; position: absolute"
+        src="https://via.placeholder.com/40x51" />
+    <div class="Rectangle14"
+        style="width: 334px; height: 66px; left: 337px; top: 533px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #969696 solid">
+    </div>
+    <div class="CNgViC4"
+        style="width: 334px; height: 67px; left: 337px; top: 533px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Công việc 4</div>
+    <img class="CheckedCheckbox" style="width: 40px; height: 50.83px; left: 344px; top: 540px; position: absolute"
+        src="https://via.placeholder.com/40x51" />
+    <div class="Rectangle15"
+        style="width: 334px; height: 66px; left: 337px; top: 620px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #969696 solid">
+    </div>
+    <div class="CNgViC5"
+        style="width: 334px; height: 67px; left: 337px; top: 620px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Công việc 5</div>
+    <img class="CheckedCheckbox" style="width: 40px; height: 50.83px; left: 344px; top: 627px; position: absolute"
+        src="https://via.placeholder.com/40x51" />
+    <div class="Rectangle16"
+        style="width: 334px; height: 66px; left: 337px; top: 707px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #969696 solid">
+    </div>
+    <div class="Rectangle25"
+        style="width: 1042px; height: 190px; left: 337px; top: 794px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #969696 solid">
+    </div>
+    <div class="CNgViC6"
+        style="width: 334px; height: 67px; left: 337px; top: 707px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Công việc 6</div>
+    <img class="CheckedCheckbox" style="width: 40px; height: 50.83px; left: 344px; top: 714px; position: absolute"
+        src="https://via.placeholder.com/40x51" />
+    <img class="Image6" style="width: 65px; height: 58px; left: 707px; top: 279px; position: absolute"
+        src="https://via.placeholder.com/65x58" />
+    <div class="Rectangle17"
+        style="width: 334px; height: 67px; left: 691px; top: 359px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #848484 solid">
+    </div>
+    <div class="Rectangle18"
+        style="width: 334px; height: 67px; left: 691px; top: 446px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #848484 solid">
+    </div>
+    <div class="Rectangle19"
+        style="width: 334px; height: 67px; left: 691px; top: 533px; position: absolute; background: white; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.20); border-radius: 10px; border: 1px #848484 solid">
+    </div>
+    <img class="Image133" style="width: 65px; height: 58px; left: 707px; top: 537px; position: absolute"
+        src="https://via.placeholder.com/65x58" />
+    <img class="Image158" style="width: 65px; height: 58px; left: 707px; top: 625px; position: absolute"
+        src="https://via.placeholder.com/65x58" />
+    <div class="HNhPhC"
+        style="width: 235px; height: 63px; left: 790px; top: 274px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Hạnh phúc</div>
+    <div class="HNhPhC"
+        style="width: 235px; height: 63px; left: 790px; top: 274px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Hạnh phúc</div>
+    <div class="Vui"
+        style="width: 235px; height: 63px; left: 790px; top: 362px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Vui</div>
+    <div class="BNhThNg"
+        style="width: 235px; height: 63px; left: 790px; top: 449px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Bình thường</div>
+    <div class="BuN"
+        style="width: 235px; height: 63px; left: 790px; top: 537px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Buồn</div>
+    <div class="RTBuN"
+        style="width: 235px; height: 63px; left: 790px; top: 620px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Rất buồn</div>
+    <div class="5Lit"
+        style="width: 235px; height: 63px; left: 1120px; top: 275px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        0,5 Lit</div>
+    <div class="Lit"
+        style="width: 235px; height: 63px; left: 1120px; top: 363px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        1 Lit</div>
+    <div class="5Lit"
+        style="width: 235px; height: 63px; left: 1120px; top: 450px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        1,5 Lit</div>
+    <div class="Lit"
+        style="width: 235px; height: 63px; left: 1120px; top: 538px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        2 Lit</div>
+    <div class="5Lit"
+        style="width: 235px; height: 63px; left: 1120px; top: 621px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        2,5 Lit</div>
+    <img class="Done" style="width: 100px; height: 100px; left: 322px; top: 90px; position: absolute"
+        src="https://via.placeholder.com/100x100" />
+    <div class="CNgViCTrongNgY"
+        style="width: 615px; height: 105px; left: 422px; top: 91px; position: absolute; color: black; font-size: 28px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Công việc trong ngày</div>
+    <div class="Rectangle26"
+        style="width: 91px; height: 34px; left: 344px; top: 803px; position: absolute; background: rgba(248.40, 235.79, 122.34, 0.77); box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15); border-radius: 8px">
+    </div>
+    <div class="GhiCh"
+        style="width: 141px; height: 56px; left: 358px; top: 792px; position: absolute; color: black; font-size: 14px; font-family: Inter; font-weight: 700; word-wrap: break-word">
+        Ghi chú :</div>
+    <div class="Line1"
+        style="width: 859px; height: 0px; left: 466px; top: 837px; position: absolute; border: 1px black dotted"></div>
+    <div class="Line2"
+        style="width: 859px; height: 0px; left: 466px; top: 887.48px; position: absolute; border: 1px black dotted">
+    </div>
+    <div class="Line3"
+        style="width: 859px; height: 0px; left: 466px; top: 937.48px; position: absolute; border: 1px black dotted">
+    </div>
+</div>
